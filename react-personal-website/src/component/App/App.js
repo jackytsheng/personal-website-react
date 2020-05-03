14647f2272a03b301811ece5e1f432a98656c282
jackytsheng <jackytsheng@gmail.com>
2020-05-04 01:33:16 +1000

    setting up react for my new personal website

---
+++ react-personal-website/src/component/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App(){
+  return (
+    <div className="layout">
+      <section className="layout__header">Hello</section>
+    </div> 
+  );
+}
+
+export default App
