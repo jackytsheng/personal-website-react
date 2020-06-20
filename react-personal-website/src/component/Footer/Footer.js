c9241bf012f8dcf68ce28b9dabd448d9d428a195
jackytsheng <jackytsheng@gmail.com>
2020-06-20 21:56:24 +1000

    changes added

--- react-personal-website/src/component/Footer/Footer.js
+++ react-personal-website/src/component/Footer/Footer.js
@@ -9,12 +9,19 @@ import {
 
 const Footer = () => (
   <div className={styles.wrapper}>
-    <Icon icon={faTwitterSquare} href="https://twitter.com/Jiajin_Zheng" />
-    <Icon
-      icon={faLinkedin}
-      href="https://www.linkedin.com/in/jiajin-zheng-76a64b164/"
-    />
-    <Icon icon={faGithubSquare} href="https://github.com/jackytsheng" />
+    <div className={styles.copyrightWrapper}>
+      <p className={styles.copyright}>
+        Copyright &copy; 2020 Jiajin Zheng. All rights reserved.
+      </p>
+    </div>
+    <div className={styles.iconWrapper}>
+      <Icon icon={faTwitterSquare} href="https://twitter.com/Jiajin_Zheng" />
+      <Icon
+        icon={faLinkedin}
+        href="https://www.linkedin.com/in/jiajin-zheng-76a64b164/"
+      />
+      <Icon icon={faGithubSquare} href="https://github.com/jackytsheng" />
+    </div>
   </div>
 );
 ;
