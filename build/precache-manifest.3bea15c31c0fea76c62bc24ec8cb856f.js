5eb4c4bb0e5728daa9c9057a2a649a34fa108600
jackytsheng <jackytsheng@gmail.com>
2020-06-25 16:38:41 +1000

    deploy to gh-pages

---
+++ build/precache-manifest.3bea15c31c0fea76c62bc24ec8cb856f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5da0ca3b98f0cf9dac7537b60a933d",
+    "url": "/personal-website-react/index.html"
+  },
+  {
+    "revision": "e50c4f9631a23645a6ed",
+    "url": "/personal-website-react/static/css/main.061f686e.chunk.css"
+  },
+  {
+    "revision": "9d660ee89f8c031fd5ce",
+    "url": "/personal-website-react/static/js/2.0a3ed377.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/personal-website-react/static/js/2.0a3ed377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50c4f9631a23645a6ed",
+    "url": "/personal-website-react/static/js/main.f32939ea.chunk.js"
+  },
+  {
+    "revision": "8d349ba4d7badee7b74d",
+    "url": "/personal-website-react/static/js/runtime-main.2e78ed41.js"
+  },
+  {
+    "revision": "914f1e2023c71f40b2b6ef4353fdac83",
+    "url": "/personal-website-react/static/media/Jiajin_Zheng_Resume.914f1e20.pdf"
+  },
+  {
+    "revision": "b69531b56b81c4e4a90f89b99f86b8ff",
+    "url": "/personal-website-react/static/media/profile.b69531b5.PNG"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.65668a202e2d7bc580d2ff68973259c0.js"
+  "/personal-website-react/precache-manifest.3bea15c31c0fea76c62bc24ec8cb856f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
