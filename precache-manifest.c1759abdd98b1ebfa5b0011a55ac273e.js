44ee350a76c0283bb93568699cd3cb16ab3b04c5
jackytsheng <jackytsheng@gmail.com>
2020-06-26 16:10:06 +1000

    Updates

---
+++ precache-manifest.c1759abdd98b1ebfa5b0011a55ac273e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fac7e4153305d62ea939e5c0d7870b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0519d02f3472135c57cb",
+    "url": "./static/css/main.38aac66e.chunk.css"
+  },
+  {
+    "revision": "f605e73b2e3c0e6f03d3",
+    "url": "./static/js/2.7454a225.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "./static/js/2.7454a225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0519d02f3472135c57cb",
+    "url": "./static/js/main.9c3738cf.chunk.js"
+  },
+  {
+    "revision": "4f2bc901ea7e2693947b",
+    "url": "./static/js/runtime-main.321de5ab.js"
+  },
+  {
+    "revision": "443024e3c88d2564cbefdaa1a32292f3",
+    "url": "./static/media/Jiajin_Zheng_Resume.443024e3.pdf"
+  },
+  {
+    "revision": "b69531b56b81c4e4a90f89b99f86b8ff",
+    "url": "./static/media/profile.b69531b5.PNG"
+  }
+]);
