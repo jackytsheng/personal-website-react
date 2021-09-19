80a064ef024f339dc061c20add34d1a2e2626c26
jackytsheng <jackytsheng@gmail.com>
2021-09-19 21:13:28 +1000

    change name to gadget

--- src/component/Page/component/Header/component/Nav/Nav.js
+++ src/component/Page/component/Header/component/Nav/Nav.js
@@ -1,32 +1,32 @@
-import React from "react";
-import Item from "./component/Item";
-import styles from "./Nav.module.scss"
+import React from 'react';
+import Item from './component/Item';
+import styles from './Nav.module.scss';
 
 const Nav = ({ current, handleClick }) => (
   <nav className={styles.container}>
     <div className={styles.topFiller}></div>
     <Item
-      href="PROFILE"
-      activeState={current === "PROFILE"}
+      href='PROFILE'
+      activeState={current === 'PROFILE'}
       onClick={handleClick}
     />
-    <Item href="HOME" activeState={current === "HOME"} onClick={handleClick} />
+    <Item href='HOME' activeState={current === 'HOME'} onClick={handleClick} />
     <Item
-      href="ABOUT"
-      activeState={current === "ABOUT"}
-      onClick={handleClick}
-    />
-    <Item
-      href="CONTACT"
-      activeState={current === "CONTACT"}
+      href='ABOUT'
+      activeState={current === 'ABOUT'}
       onClick={handleClick}
     />
     <Item
-      href="https://www.youtube.com/channel/UCvNmoHGEeNvE442gZv8qm9w"
+      href='CONTACT'
+      activeState={current === 'CONTACT'}
+      onClick={handleClick}
+    />
+    <Item
+      href='https://www.youtube.com/channel/UCvNmoHGEeNvE442gZv8qm9w'
       onClick={(e, url) => {}}
     />
-    <Item href="https://blog.jiajinzheng.com" onClick={(e, url) => {}} />
-    <Item href="https://gaget.jiajinzheng.com" onClick={(e, url) => {}} />
+    <Item href='https://blog.jiajinzheng.com' onClick={(e, url) => {}} />
+    <Item href='https://gadget.jiajinzheng.com' onClick={(e, url) => {}} />
 
     <div className={styles.bottomFiller}></div>
   </nav>
--- src/component/Page/component/Header/component/Nav/component/Item/Item.js
+++ src/component/Page/component/Header/component/Nav/component/Item/Item.js
@@ -1,37 +1,41 @@
 import React from 'react';
 import styles from './Item.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faUser,faBook,faAddressCard,faHome,faUserCircle,faGamepad} from '@fortawesome/free-solid-svg-icons'
-import {faYoutube} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faUser,
+  faBook,
+  faAddressCard,
+  faHome,
+  faUserCircle,
+  faGamepad,
+} from '@fortawesome/free-solid-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 const iconMapper = {
   HOME: faHome,
   ABOUT: faUser,
-  "https://blog.jiajinzheng.com": faBook,
-  "https://gaget.jiajinzheng.com": faGamepad,
+  'https://blog.jiajinzheng.com': faBook,
+  'https://gadget.jiajinzheng.com': faGamepad,
   CONTACT: faAddressCard,
   PROFILE: faUserCircle,
-  "https://www.youtube.com/channel/UCvNmoHGEeNvE442gZv8qm9w": faYoutube,
+  'https://www.youtube.com/channel/UCvNmoHGEeNvE442gZv8qm9w': faYoutube,
 };
-const Item = (
-  props
-) => {
-return (
-  <a
-    className={`
-    ${props.href === "PROFILE" ? styles.profile : ""}
-      ${props.href === "HOME" ? styles.itemFirst : ""} ${
-      props.activeState ? `${styles.item} ${styles.active}` : styles.item
-    }`}
-    href={props.href}
-    onClick={(e) => props.onClick(e, props.href)}
-    rel="noopener noreferrer"
-    target="_blank"
-  >
-    <FontAwesomeIcon icon={iconMapper[props.href]} />
-  </a>
-);
+const Item = (props) => {
+  return (
+    <a
+      className={`
+    ${props.href === 'PROFILE' ? styles.profile : ''}
+      ${props.href === 'HOME' ? styles.itemFirst : ''} ${
+        props.activeState ? `${styles.item} ${styles.active}` : styles.item
+      }`}
+      href={props.href}
+      onClick={(e) => props.onClick(e, props.href)}
+      rel='noopener noreferrer'
+      target='_blank'
+    >
+      <FontAwesomeIcon icon={iconMapper[props.href]} />
+    </a>
+  );
 };
-
 
 export default Item;
